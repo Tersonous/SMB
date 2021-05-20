b2ba061f47c3f11f66d2fb3ef52dc978acc81a6c
Tersonous <dylan.360@hotmail.fr>
2021-05-20 09:51:26 +0200

    Add files via upload

--- toktest.js
+++ toktest.js
@@ -4,24 +4,21 @@
 
 async function main() {
     console.log('Run test: SMBT1');
-
+    // initialisation SMT1
+    // Propriétaite initial du token
+    let initialOwnerTokenAccount;
+    const solanaWeb3 = require('@solana/web3.js');
+    const { Console } = require('console');
+    console.log(solanaWeb3);
+    const connection = new solanaWeb3.Connection('https://devnet.solana.com');
+    const accountOfToken = new solanaWeb3.Account();
+    connection.requestAirdrop(accountOfToken.publicKey);
+    console.log(connection.getTokenSupply(accountOfToken.publicKey));
+    SMBT1 = new Token(initialOwnerTokenAccount);
     
   }
 
-  main()
-  .catch(err => {
-    console.error(err);
-  })
-  .then(() => process.exit());
-
-
-// Propriétaite initial du token
-let initialOwnerTokenAccount;
-const solanaWeb3 = require('@solana/web3.js');
-const { Console } = require('console');
-console.log(solanaWeb3);
-const connection = new solanaWeb3.Connection('https://devnet.solana.com');
-const accountOfToken = new solanaWeb3.Account();
+ 
 
 
 
@@ -44,13 +41,13 @@ class Token {
 
 }
 
-class TokenNFT {
-    //ERC 721
-}
 
-// initialisation SMT1
-SMBT1 = new Token(initialOwnerTokenAccount);
 
+main()
+.catch(err => {
+  console.error(err);
+})
+.then(() => process.exit());
 
 
 
